c69c02282273e841faa7093929fb58b6f841a564
RandomB_oi <randomboi.personal+github@gmail.com>
2024-02-01 15:33:05 -0600

    a

--- Framework/Vector2.lua
+++ Framework/Vector2.lua
@@ -5,10 +5,42 @@ module.new = function(x,y)
     local self = setmetatable({}, module)
     self.X = x or 0
     self.Y = y or 0
-    self.Magnitude = math.sqrt(self.X ^ 2 + self.Y ^ 2)
-    self.Unit = setmetatable({X = self.X / self.Magnitude, Y = self.Y / self.Magnitude})
 
     return self
+end
+
+function module:Length()
+    return math.sqrt(self.X ^ 2 + self.Y ^ 2)
+end
+
+function module:Unit()
+    return self/self:Length()
+end
+
+function module:__add(other)
+    return module.new(self.X + other.X, self.Y + other.Y)
+end
+
+function module:__sub(other)
+    return module.new(self.X - other.X, self.Y - other.Y)
+end
+
+function module:__div(other)
+    if type(self) == "number" then return other/self end
+
+    if type(other) == "number" then
+        return module.new(self.X / other, self.Y / other)
+    end
+    return module.new(self.X / other.X, self.Y / other.Y)
+end
+
+function module:__mul(other)
+    if type(self) == "number" then return other/self end
+
+    if type(other) == "number" then
+        return module.new(self.X * other, self.Y * other)
+    end
+    return module.new(self.X * other.X, self.Y * other.Y)
 end
 
 return module
